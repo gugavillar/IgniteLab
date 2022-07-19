aa10d47d3f80dfd075acc0c3534ef6ef9448da20
Guga Villar <grsv.21@gmail.com>
2022-07-19 05:49:53 -0300

    feat: criação do componente info

---
+++ src/components/Info/index.tsx
@@ -0,0 +1,11 @@
+export const Info = () => {
+  return (
+    <div className='flex flex-1'>
+      <div className='mx-auto my-auto'>
+        <h1 className='text-center font-bold text-3xl'>Bem vindo!</h1>
+        <p className='text-center text-xl my-4'>Para começar necessário selecionar uma aula ao lado.</p>
+        <p className='text-center text-xl'>Aproveite o conteúdo e bons estudos.</p>
+      </div>
+    </div>
+  )
+}
